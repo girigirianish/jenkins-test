f515e7b2879ffc303b1e6f2131f7057b97e8df9e
girigirianish <giri.girianish@gmail.com>
2021-03-18 21:37:00 +0545

    Create qualiti-script.sh

---
+++ qualiti-script.sh
@@ -0,0 +1,49 @@
+set -ex
+ 
+PROJECT_ID='332'
+API_KEY='<your-api-key>'
+CLIENT_ID='<your-client-id>'
+SCOPES=['"ViewTestResults"','"ViewAutomationHistory"']
+API_URL='https://7iggpnqgq9.execute-api.us-east-2.amazonaws.com/udbodh/api'
+INTEGRATION_JWT_TOKEN='<your-jwt-token>'
+INTEGRATIONS_API_URL='<ngrok-url>'
+ 
+
+apt-get update
+apt-get install -y jq
+ 
+#Trigger test run
+TEST_RUN_ID="$( \
+  curl -X POST -G ${INTEGRATIONS_API_URL}/api/integrations/gitlab/${PROJECT_ID}/events \
+    -d 'token='$INTEGRATION_JWT_TOKEN''\
+    -d 'triggerType=Deploy'\
+  | jq -r '.test_run_id')"
+ 
+AUTHORIZATION_TOKEN="$( \
+  curl -X POST -G ${API_URL}/auth/token \
+  -H 'x-api-key: '${API_KEY}'' \
+  -H 'client_id: '${CLIENT_ID}'' \
+  -H 'scopes: '${SCOPES}'' \
+  | jq -r '.token')"
+ 
+# Wait until the test run has finished
+while : ; do
+   RESULT="$( \
+   curl -X GET ${API_URL}/automation-history?project_id=${PROJECT_ID}\&test_run_id=${TEST_RUN_ID} \
+   -H 'token: Bearer '$AUTHORIZATION_TOKEN'' \
+   -H 'x-api-key: '${API_KEY}'' \
+  | jq -r '.[0].finished')"
+  if [ "$RESULT" != null ]; then
+    break;
+  fi
+    sleep 15;
+done
+ 
+# # Once finished, verify the test result is created and that its passed
+TEST_RUN_RESULT="$( \
+  curl -X GET ${API_URL}/test-results?test_run_id=${TEST_RUN_ID}\&project_id=${PROJECT_ID} \
+    -H 'token: Bearer '$AUTHORIZATION_TOKEN'' \
+    -H 'x-api-key: '${API_KEY}'' \
+  | jq -r '.[0].status' \
+)"
+echo "Qualiti E2E Tests ${TEST_RUN_RESULT}"
