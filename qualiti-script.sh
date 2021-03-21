f34fe1028fa5a9400c7a5d179049729ebed2eb1f
girigirianish <giri.girianish@gmail.com>
2021-03-22 02:17:36 +0545

    Update qualiti-script.sh

--- qualiti-script.sh
+++ qualiti-script.sh
@@ -1,31 +1,33 @@
+#!/bin/bash
+ 
 set -ex
  
 PROJECT_ID='332'
-API_KEY='<your-api-key>'
-CLIENT_ID='<your-client-id>'
+API_KEY='C3GVjpEWgX5mSTBh7XcmV4AMqcBTrGst2W65FKal'
+CLIENT_ID='f2b2cea4ac9980f694f7c254ea33e2f9'
 SCOPES=['"ViewTestResults"','"ViewAutomationHistory"']
 API_URL='https://7iggpnqgq9.execute-api.us-east-2.amazonaws.com/udbodh/api'
-INTEGRATION_JWT_TOKEN='<your-jwt-token>'
-INTEGRATIONS_API_URL='<ngrok-url>'
+INTEGRATION_JWT_TOKEN='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0X2lkIjozMzIsImFwaV9rZXlfaWQiOjI0ODMsIm5hbWUiOiIiLCJkZXNjcmlwdGlvbiI6IiIsImljb24iOiIiLCJpbnRlZ3JhdGlvbl9uYW1lIjoiR2l0bGFiIiwib3B0aW9ucyI6e30sImlhdCI6MTYxNjE1NjI2OX0.F4C59OAbjnhoA9OK_5MRoDlbL-4AVNiu3rD0S6Khj7c'
+INTEGRATIONS_API_URL='http://817b74f0cd89.ngrok.io'
  
-
-apt-get update
+apt-get update -y
 apt-get install -y jq
- 
+
 #Trigger test run
 TEST_RUN_ID="$( \
   curl -X POST -G ${INTEGRATIONS_API_URL}/api/integrations/gitlab/${PROJECT_ID}/events \
     -d 'token='$INTEGRATION_JWT_TOKEN''\
     -d 'triggerType=Deploy'\
+    -d 'projectId='$PROJECT_ID''\
   | jq -r '.test_run_id')"
- 
+
 AUTHORIZATION_TOKEN="$( \
   curl -X POST -G ${API_URL}/auth/token \
   -H 'x-api-key: '${API_KEY}'' \
   -H 'client_id: '${CLIENT_ID}'' \
   -H 'scopes: '${SCOPES}'' \
   | jq -r '.token')"
- 
+
 # Wait until the test run has finished
 while : ; do
    RESULT="$( \
@@ -33,12 +35,15 @@ while : ; do
    -H 'token: Bearer '$AUTHORIZATION_TOKEN'' \
    -H 'x-api-key: '${API_KEY}'' \
   | jq -r '.[0].finished')"
+  # echo $RESULT
   if [ "$RESULT" != null ]; then
+    echo "the result is not null : $RESULT"
     break;
   fi
+    echo "the result is : $RESULT"
     sleep 15;
 done
- 
+
 # # Once finished, verify the test result is created and that its passed
 TEST_RUN_RESULT="$( \
   curl -X GET ${API_URL}/test-results?test_run_id=${TEST_RUN_ID}\&project_id=${PROJECT_ID} \
